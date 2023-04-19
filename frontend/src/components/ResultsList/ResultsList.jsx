886845759110cbd87bbb01ce88c8ce7d9d4db2b0
Backinblacker <coachblacker@gmail.com>
2023-04-19 10:01:03 -0600

    📦 NEW: Added ResultsList Component

---
+++ frontend/src/components/ResultsList/ResultsList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './ResultsList.css';
+const ResultsList = (props) => {
+    return (  
+        <div className='Todo'>
+            <h2>This is the Results List</h2>
+        </div>
+    );
+}
+ 
+export default ResultsList;
