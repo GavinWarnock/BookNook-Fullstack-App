c7673f20de8451a0003b1c98f6ccd4f408626fc1
GavinWarnock <gbwarnock@gmail.com>
2023-04-20 10:19:41 -0600

    👌 IMPROVE: Added book picture to search results

--- frontend/src/pages/BookDetailPage/BookDetailPage.js
+++ frontend/src/pages/BookDetailPage/BookDetailPage.js
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 
 const BookDetailPage = ({ searchResults }) => {
     const {bookid} = useParams()
+    
     return (  
         <div className='resultsList'>
             <h2>Here is where your book search is displayed</h2>
