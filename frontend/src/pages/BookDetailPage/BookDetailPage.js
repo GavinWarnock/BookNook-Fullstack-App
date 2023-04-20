4d340809562bc510060b85cbb459363ed476da29
GavinWarnock <gbwarnock@gmail.com>
2023-04-20 14:02:33 -0600

    👌 IMPROVE: We were able to show the title of the book we searched

--- frontend/src/pages/BookDetailPage/BookDetailPage.js
+++ frontend/src/pages/BookDetailPage/BookDetailPage.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import "./BookDetailPage.css";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
-const BookDetailPage = ({ searchResults }) => {
-    const {bookid} = useParams()
-    
-    return (  
-        <div className='resultsList'>
-            <h2>Here is where your book search is displayed</h2>
-        </div>
-    );
-}
+const BookDetailPage = () => {
+  const { bookid } = useParams();
+  const [bookDetails, setBookDetails] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchBookDetails = async () => {
+    try {
+      //Make axios request
+      let response = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes/${bookid}`
+      );
+      setBookDetails(response.data);
+      setIsLoading(false)
+    } catch (error) {
+      console.log("Error in fetchBooksDetails:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchBookDetails();
+  }, []);
  
+
+  return (
+    <div className="resultsList">
+      <h2>Details {bookid} </h2>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+            Description: {bookDetails.volumeInfo.title}
+        </div>)}
+    </div>
+  );
+};
+
 export default BookDetailPage;
