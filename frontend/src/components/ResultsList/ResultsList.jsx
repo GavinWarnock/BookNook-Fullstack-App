4b5107c8202a2848dff22498a4b5278de2c50987
Backinblacker <coachblacker@gmail.com>
2023-04-20 10:52:41 -0600

    👌 IMPROVE: Added Conditional Statement to handle when an image is not present.

--- frontend/src/components/ResultsList/ResultsList.jsx
+++ frontend/src/components/ResultsList/ResultsList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./ResultsList.css";
 import { Link } from "react-router-dom";
+
 const ResultsList = ({ searchResults }) => {
   return (
     <div className="resultsList">
@@ -9,7 +10,11 @@ const ResultsList = ({ searchResults }) => {
         <Link key={book.id} to={`/details/${book.id}`}>
           <div>
             <h3>{book?.volumeInfo.title}</h3>
-            <img src={book?.volumeInfo.imageLinks.smallThumbnail}/>
+            {book?.volumeInfo.imageLinks ? (
+              <img src={book?.volumeInfo.imageLinks.smallThumbnail} alt={book?.volumeInfo.title}/>
+            ) : (
+              <div>No Image Available</div>
+            )}
             <h3>Average Rating: {book?.volumeInfo.averageRating} </h3>
           </div>
         </Link>
