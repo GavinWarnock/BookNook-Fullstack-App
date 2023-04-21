4c65016385b6c201a8230556ce0fbe75c693af4c
Backinblacker <coachblacker@gmail.com>
2023-04-21 10:34:50 -0600

    👌 IMPROVE: moved favorite book to its own page

---
+++ frontend/src/components/FavoriteBooks/FavoriteBooks.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+const FavoriteBooks=({auth}) => {
+
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    fetchFavorites();
+        }, []);
+    async function fetchFavorites (){
+      try {
+        let response = await axios.get("http://127.0.0.1:5000/api/user_favorites", {
+          headers: {
+            Authorization: auth,
+          },
+        });
+        setFavorites(response.data);
+      } catch (error) {
+        console.log(error.response.data);
+      }
+          };
+
+    return (
+        <div>
+            {favorites.length > 0 ?
+            favorites.map((bookDetails, index) => {
+            return(
+                <Link key={index} to={`/detail/${bookDetails.book_id}`}>
+                    <div className='favorite-card'>
+                        <img src={bookDetails.thumbnail_url} alt={`${bookDetails.title}`} />
+                        <h2>{bookDetails.title}</h2>
+                        <button>{'<3'}</button>
+                    </div>
+                </Link>
+            )
+        })
+        :null}
+    </div>
+  );
+};
+
+export default FavoriteBooks;
